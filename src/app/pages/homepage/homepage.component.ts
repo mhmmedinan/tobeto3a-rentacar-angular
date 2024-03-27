21d2509e95b0a6cd061f60dab2fc515bad7e1365
Muhammed İnan <minan.65@hotmail.com>
2024-03-27 15:34:31 +0300

    27.03.2024 commit 2

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,13 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,CommonModule,HttpClientModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+  todoList:string[]=[
+    // 'todolist 1',
+    // 'todolist 2',
+    // 'todolist 3'
+  ];
+
+  constructor(private httpClient:HttpClient){}
+
+  ngOnInit(): void {
+    this.getTodos2();
+  }
+
+ 
+
+
+  getTodos1(){
+
+    console.log("Öncesi")
+    this.asyncOperation()
+    .then((response:string)=>{
+      console.log("Doğru çalıştı : ",response)
+    })
+    .catch((error)=>{
+      console.log("Hata :",error)
+    })
+    .finally(()=>{
+      console.log("Başarılı veya başarısız");
+    })
+    console.log("sonrası")
+  }
+
+  async getTodos2(){
+    try {
+      let values = await this.asyncOperation();
+      console.log(values);
+    } catch (error) {
+      console.log("hata :",error)
+    }
+  }
+
+  asyncOperation():Promise<string>{
+    //callback function
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        reject("çalıştı");
+      })
+    });
+  }
 
 }
